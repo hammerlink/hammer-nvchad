ca225867bcd6c0c5c6805f17d91ead70b177b553
hendrik <hendrik.hamerlinck@hammernet.be>
2025-01-26 17:35:22 +0100

    extract telescope config & add additional mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,7 @@ local map = vim.keymap.set
 -- escape modus
 map('i', 'fd', '<ESC>', { desc = 'custom escape' })
 map('v', 'fd', '<ESC>', { desc = 'custom escape' })
+map('n', '<leader>tx', '<cmd> tabclose <CR>', { desc = 'Tab close' })
 
 -- neogit
 map('n', '<leader>gg', '<cmd> Neogit <cr>', { desc = 'Open NeoGit' })
@@ -15,6 +16,9 @@ map('n', '<leader>gg', '<cmd> Neogit <cr>', { desc = 'Open NeoGit' })
 map('n', '<leader>fr', '<cmd> Telescope resume <cr>', { desc = 'Resume last find' })
 map('n', '<leader>fj', '<cmd> Telescope jump_list <cr>', { desc = 'Find recent file visits' })
 map('n', '<leader>fn', '<cmd> Telescope notify <cr>', { desc = 'Browse notifications' })
+map('n', '<leader>fx', function()
+    require('telescope.builtin').diagnostics { bufnr = 0 }
+end, { desc = 'Find diagnostics' })
 
 -- LSP
 map('n', '<leader>li', '<cmd> LspInfo <cr>', { desc = 'LSP Info' })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -32,17 +32,5 @@ return {
         },
     },
     require 'lua.plugins.neogit',
-    {
-        'nvim-telescope/telescope.nvim',
-        opts = function(_, conf)
-            conf.defaults.mappings.i = {
-                ['<C-j>'] = require('telescope.actions').move_selection_next,
-                ['<Esc>'] = require('telescope.actions').close,
-            }
-
-            -- or
-            -- table.insert(conf.defaults.mappings.i, your table)
-            return conf
-        end,
-    },
+    require 'lua.plugins.telescope',
 }
