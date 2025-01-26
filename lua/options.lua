8f7764a51e9d6e22ac6c44a21cc3d2da875acb9d
hendrik <hendrik.hamerlinck@hammernet.be>
2025-01-26 17:23:37 +0100

    add vim base options

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,20 @@
 require "nvchad.options"
 
+local opt = vim.opt
+
+-- Indenting
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+
+-- Numbers
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 2
+opt.ruler = false
+
 -- add yours here!
 
 -- local o = vim.o
