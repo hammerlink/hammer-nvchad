62c37f30ae717e9e21b0edf1932415fd789785fa
hendrik <hendrik.hamerlinck@hammernet.be>
2025-01-26 17:19:52 +0100

    add lsp mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,5 +16,9 @@ map("n", "<leader>fr", "<cmd> Telescope resume <cr>", { desc = "Resume last find
 map("n", "<leader>fj", "<cmd> Telescope jump_list <cr>", { desc = "Find recent file visits" })
 map("n", "<leader>fn", "<cmd> Telescope notify <cr>", { desc = "Browse notifications" })
 
+-- LSP
+map("n", "<leader>li", "<cmd> LspInfo <cr>", { desc = "LSP Info" })
+map("n", "<leader>lr", "<cmd> LspRestart <cr>", { desc = "LSP Restart" })
+
 -- Disable mappings
 local nomap = vim.keymap.del
