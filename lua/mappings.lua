d44427a86ee0140289e1de953c28c5dc8ecff02e
hendrik <hendrik.hamerlinck@hammernet.be>
2025-01-26 17:11:45 +0100

    add telescope mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,11 +5,16 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 -- escape modus
-map("i", "jk", "<ESC>", { desc = "custom escape" })
-map("v", "jk", "<ESC>", { desc = "custom escape" })
+map("i", "fd", "<ESC>", { desc = "custom escape" })
+map("v", "fd", "<ESC>", { desc = "custom escape" })
 
 -- neogit
 map("n", "<leader>gg", "<cmd> Neogit <cr>", { desc = "Open NeoGit" })
+
+-- telescope
+map("n", "<leader>fr", "<cmd> Telescope resume <cr>", { desc = "Resume last find" })
+map("n", "<leader>fj", "<cmd> Telescope jump_list <cr>", { desc = "Find recent file visits" })
+map("n", "<leader>fn", "<cmd> Telescope notify <cr>", { desc = "Browse notifications" })
 
 -- Disable mappings
 local nomap = vim.keymap.del
