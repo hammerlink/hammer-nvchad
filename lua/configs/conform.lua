4d5396b11f73f15fac42df96242a4148a8807af2
hendrik <hendrik.hamerlinck@hammernet.be>
2025-01-30 10:13:20 +0100

    enable prettier for js, ts & respect the ignore file

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,33 @@
 local options = {
     formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
         -- css = { "prettier" },
         -- html = { "prettier" },
+    },
+
+    formatters = {
+        prettier = {
+            -- This will make prettier respect .prettierignore
+            cwd = require('conform.util').root_file {
+                '.prettierrc',
+                '.prettierrc.json',
+                '.prettierrc.js',
+                'prettier.config.js',
+            },
+            -- Alternative: find git root
+            -- cwd = require("conform.util").root_file(".git"),
+
+            -- Explicitly tell prettier to use .prettierignore
+            args = {
+                '--ignore-path',
+                '.prettierignore',
+                -- Add your other prettier args here
+                '--stdin-filepath',
+                '$FILENAME',
+            },
+        },
     },
 
     -- format_on_save = {
