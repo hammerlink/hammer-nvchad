87208356e25939f661e2dc89a0ec34b66e7f5518
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-10-08 10:37:22 +0200

    customize the nvim-cmp key-bindings

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,18 @@ return {
         -- event = 'BufWritePre', -- uncomment for format on save
         opts = require "configs.conform",
     },
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function()
+            local cmp = require "nvchad.configs.cmp"
+            local custom_cmp = require "configs.cmp"
+
+            -- Merge the custom mappings with default ones
+            cmp.mapping = vim.tbl_deep_extend("force", cmp.mapping, custom_cmp.mapping)
+
+            return cmp
+        end,
+    },
 
     -- These are some examples, uncomment them if you want to see them work!
     {
